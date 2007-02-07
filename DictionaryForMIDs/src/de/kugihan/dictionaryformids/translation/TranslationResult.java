7c7c80fe6d2ca91b65dc6b86b3b6ac8f45ae85ba
tunichtviel <tunichtviel@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2007-02-07 20:24:21 +0000

    Correction of a bug where for single hits 'not found' was shown

--- DictionaryForMIDs/src/de/kugihan/dictionaryformids/translation/TranslationResult.java
+++ DictionaryForMIDs/src/de/kugihan/dictionaryformids/translation/TranslationResult.java
@@ -10,17 +10,39 @@ import java.util.*;
 
 public class TranslationResult {
 
-	public boolean translationFound; // true if a valid translation was found in the dictionary
-	public long    numberOfHits;  // indicates how many times a given search expression was found in the dictionary
-	public long    executionTime; // in ms
-	
+	public long    executionTime; 			 // in ms
 	public boolean translationBreakOccurred = false; // true if the translation was aborted
 	public int     translationBreakReason; // reason why translation was aborted
 	
-	public Vector  translations = new Vector();  // Vector of SingleTranslation
+	protected Vector  translations = new Vector();  // Vector of SingleTranslation
 	
 	public final static int BreakReasonCancelReceived = 1; 
 	public final static int BreakReasonMaxExecutionTimeReached = 2; 
 	public final static int BreakReasonCancelMaxNrOfHitsReached = 3; 
+	
+	public SingleTranslation getTranslationAt(int index) {
+		return (SingleTranslation) translations.elementAt(index);
+	}
+
+	public Enumeration getAllTranslations() {
+		return translations.elements();
+	}
+
+	public int numberOfFoundTranslations() {
+		return translations.size();
+	}
+
+	public boolean translationFound() {
+		return numberOfFoundTranslations() >0;
+	}
+
+	public void addTranslation(SingleTranslation newSingleTranslation) {
+		translations.addElement(newSingleTranslation);
+	}
+
+	public void insertTranslationAt(SingleTranslation newSingleTranslation, int index) {
+		translations.insertElementAt(newSingleTranslation, index);
+	}
+
 }
 
