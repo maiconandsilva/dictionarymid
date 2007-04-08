eda6816abefc4ba9bc88465ded351fe7ad5e77b2
tunichtviel <tunichtviel@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2007-04-08 19:08:06 +0000

    Added CommandBack

--- trunk/DictionaryForMIDs/src/de/kugihan/dictionaryformids/hmi_java_me/uidisplaytext/UIDisplayTextItems.java
+++ trunk/DictionaryForMIDs/src/de/kugihan/dictionaryformids/hmi_java_me/uidisplaytext/UIDisplayTextItems.java
@@ -87,4 +87,5 @@ public class UIDisplayTextItems {
 	public static final UIDisplayTextItem StartDisplaySizeOfDictionaryInKb = LanguageUI.getUI().createNewUIDisplayTextItem("StartDisplaySizeOfDictionaryInKb", 67, 0);
 	public static final UIDisplayTextItem DictionaryForMIDsApplicationName = LanguageUI.getUI().createNewUIDisplayTextItem("DictionaryForMIDsApplicationName", 68, 0);
 	public static final UIDisplayTextItem MessageNoDictionaryLoaded = LanguageUI.getUI().createNewUIDisplayTextItem("MessageNoDictionaryLoaded", 69, 0);
+	public static final UIDisplayTextItem CommandBack = LanguageUI.getUI().createNewUIDisplayTextItem("CommandBack", 70, 0);
 }
