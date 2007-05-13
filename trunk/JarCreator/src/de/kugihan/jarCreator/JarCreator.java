f5533d1a47cefbf5ffac8298afafeaef64b63d2d
tunichtviel <tunichtviel@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2007-05-13 09:45:25 +0000

    Copy recursively all content of the dictionarydirectory to the JAR-file

--- trunk/JarCreator/src/de/kugihan/jarCreator/JarCreator.java
+++ trunk/JarCreator/src/de/kugihan/jarCreator/JarCreator.java
@@ -170,21 +170,33 @@ public static final String FILE_EMPTY_JAD_NAME = DictionaryDataFile.applicationF
 	        } 
 		}
 		in.close();
+
+		addDirectory(out, dictDir, "");
 		
+		out.close();
+		return jarOutputFile.length();
+	}
+	
+	static void addDirectory(JarOutputStream out, File dictDir, String subDir) throws IOException {
+		byte[] b = new byte[3000];
+		int readBytes;
 		File[] dictFiles= dictDir.listFiles();
 		for(int i=0; i<dictFiles.length; i++) 
 		{ 
 			if (!dictFiles[i].isDirectory()){
 			      FileInputStream fis = new FileInputStream(dictFiles[i]); 						
-							out.putNextEntry(new ZipEntry(DictionaryDataFile.pathNameDataFiles + "/"+dictFiles[i].getName())); 
+							out.putNextEntry(new ZipEntry(DictionaryDataFile.pathNameDataFiles + "/"+subDir +dictFiles[i].getName())); 
 							while((readBytes = fis.read(b)) != -1) { 
 									out.write(b, 0, readBytes); 
 							}						
 			      fis.close(); 
 			}
+			else {
+				File subDirFile = dictFiles[i];
+				String newSubDir = subDir + subDirFile.getName() + '/' ;
+				addDirectory(out, subDirFile, newSubDir);
+			}
 		}
-		out.close();
-		return jarOutputFile.length();
 	}
 	
 	// adds a directory separator character to pathName if the pathName does have one at the end 
