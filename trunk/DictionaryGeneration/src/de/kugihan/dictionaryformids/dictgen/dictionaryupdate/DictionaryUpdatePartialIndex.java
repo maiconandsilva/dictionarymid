67c1cf4cf685c554721a3785575e723b2c909cf2
tunichtviel <tunichtviel@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2010-05-13 10:05:28 +0000

    - Included the behaviour from class DictionaryUpdatePartialIndex in class DictionaryUpdate - Preparation of setting language specific icons instead of the standard flags (not yet supported by any HMI layer) - Fixed small bug in detecting empty property values from DictionaryForMIDs.properties (thanks to Jacob)

--- trunk/DictionaryGeneration/src/de/kugihan/dictionaryformids/dictgen/dictionaryupdate/DictionaryUpdatePartialIndex.java
+++ trunk/DictionaryGeneration/src/de/kugihan/dictionaryformids/dictgen/dictionaryupdate/DictionaryUpdatePartialIndex.java
@@ -5,69 +5,15 @@ Copyright (C) 2005, 2006  Gert Nuber (
 
 GPL applies - see file COPYING for copyright statement.
 */
+/*
+ * Note: this class is obsolete starting with DictionaryGeneration 3.5.0 because
+ * with version 3.5.0 of DictionayGeneration the behaviour from DictionaryUpdatePartialIndex
+ * is already included in class DictionaryUpdate. The class DictionaryUpdatePartialIndex
+ * is only retained for compatibility reasons.
+ */
 package de.kugihan.dictionaryformids.dictgen.dictionaryupdate;
 
-import de.kugihan.dictionaryformids.dictgen.DictionaryGeneration;
-import de.kugihan.dictionaryformids.dictgen.dictionaryupdate.DictionaryUpdateEngDef;
-import de.kugihan.dictionaryformids.general.DictionaryException;
-import de.kugihan.dictionaryformids.general.Util;
 
 public class DictionaryUpdatePartialIndex extends DictionaryUpdate {
 	
-	final String delimiterStart = "{{";
-	final String delimiterEnd = "}}";
-
-	public String createKeyWordsExpression(String expression)  
-		throws DictionaryException {
-		String keyWordsExpressionWithoutContentDelimiters = 
-     		DictionaryGeneration.removeContentDelimiters(expression, indexLanguage);
-		StringBuffer keyWordsExpressionWithoutSeparatorChars = new StringBuffer(keyWordsExpressionWithoutContentDelimiters); 
-		Util.getUtil().replaceFieldAndLineSeparatorChars(keyWordsExpressionWithoutSeparatorChars);
-		String keyWordsCleanedUp = removeNonSearchParts(keyWordsExpressionWithoutSeparatorChars.toString());
-		return keyWordsCleanedUp;
-	}
-	
-	public String updateDictionaryExpression(String dictionaryExpression) 
-				throws DictionaryException {
-		boolean replacementDone;
-		StringBuffer expressionUpdated = new StringBuffer(dictionaryExpression);
-		do {
-			replacementDone = false;
-			// remove all delimiterStart and delimiterEnd
-			int posDelimiterStart = expressionUpdated.toString().indexOf(delimiterStart);
-			if (posDelimiterStart >= 0) {
-				expressionUpdated.delete(posDelimiterStart, posDelimiterStart + delimiterStart.length());
-				replacementDone = true;
-			}
-			else {
-				int posDelimiterEnd = expressionUpdated.toString().indexOf(delimiterEnd);
-				if (posDelimiterEnd >= 0) {
-					expressionUpdated.delete(posDelimiterEnd, posDelimiterEnd + delimiterEnd.length());
-					replacementDone = true;					
-				}
-			}
-		}
-		while (replacementDone);
-		String returnString = super.updateDictionaryExpression(expressionUpdated.toString());
-		return returnString;
-	}
-		
-	public String removeNonSearchParts(String expression) 
-			throws DictionaryException {
-		boolean replacementDone;
-		StringBuffer expressionUpdated = new StringBuffer(expression);
-		do {
-			replacementDone = false;
-			// remove everything between delimiterStart and delimiterEnd
-			int posDelimiterStart = expressionUpdated.toString().indexOf(delimiterStart);
-			int posDelimiterEnd = expressionUpdated.toString().indexOf(delimiterEnd);
-			if ((posDelimiterStart >= 0) && (posDelimiterEnd > posDelimiterStart)) {
-				expressionUpdated.delete(posDelimiterStart, posDelimiterEnd + delimiterEnd.length());
-				replacementDone = true;
-			}
-		}
-		while (replacementDone);
-		String returnString = super.removeNonSearchParts(expressionUpdated.toString());
-		return returnString;
-	}
 }
