64927e2111bccee135fb1bacd481235a209315d4
tunichtviel <tunichtviel@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2010-12-23 15:32:13 +0000

    Inititial version of the Javascript Translation Layer. Plus two php-scripts.

---
+++ trunk/htmlApp/hmi_loader.php
@@ -0,0 +1,4 @@
+<?
+		$a = file_get_contents("http://dictionarymid.sourceforge.net/htmlApp/htmlApp/mini_hmi/mini_hmi.html");
+		echo $a;
+?>
---
+++ trunk/htmlApp/index.php
@@ -0,0 +1,4 @@
+<?
+	$a = file_get_contents("http://dictionarymid.sourceforge.net/htmlApp/htmlApp/hmi_loader.php");
+	echo $a;
+?>
