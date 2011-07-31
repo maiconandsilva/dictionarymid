2e8ccc5e8d8369fa80e6d0b20396dfd7580e94b9
tunichtviel <tunichtviel@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2011-07-31 18:47:59 +0000

    Common files to which links are created from the dictionaries.

---
+++ trunk/WebApp/Apps/dictionary_common/index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+	<script type="text/javascript">
+		"use strict";
+		function redirectToHMI() {
+			window.location.href = "mini_hmi.php";
+		}
+		window.onload = redirectToHMI; 
+    </script>
+  </head>
+  <body>
+  </body>
+</html>
---
+++ trunk/WebApp/Apps/dictionary_common/mini_hmi.php
@@ -0,0 +1,4 @@
+<?
+		$a = file_get_contents("http://dictionarymid.sourceforge.net/WebApp/Apps/mini_hmi/mini_hmi.html");
+		echo $a;
+?>
