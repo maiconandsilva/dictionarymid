d689af39fdab742a55e7dbf49dc99792817faca3
karimoune <karimoune@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2013-09-21 14:28:56 +0000

    Updated Helper Scripts

---
+++ DictionaryForMIDs-Creator/res/Helper-Scripts/rsync_sf_up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Example of uploading using rsync
+# See 'man rsync' on a Linux box for more details
+
+rsync -ev ssh dfm-creator-0.6-beta-linux-amd64-with-jre.run \
+dfm-creator-0.6-beta-linux-x86-with-jre.run \
+dfm-creator-0.6-beta-x86-installer.exe \
+dfm-creator-0.6-beta-x86_64-installer.exe \
+karimoune,dictionarymid@frs.sourceforge.net:/home/project-web/dictionarymid/htdocs/DfM-Creator/files/
+
