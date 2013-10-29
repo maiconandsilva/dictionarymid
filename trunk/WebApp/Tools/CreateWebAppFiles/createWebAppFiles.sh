0f519d2824f633e92b7f5abb65446a008f4d0a31
tunichtviel <tunichtviel@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2013-10-29 17:01:26 +0000

    - Added missing files to mini_hmi manifest - Updated shell script for creating web app dictionary files

--- trunk/WebApp/Tools/CreateWebAppFiles/createWebAppFiles.sh
+++ trunk/WebApp/Tools/CreateWebAppFiles/createWebAppFiles.sh
@@ -23,6 +23,15 @@ unzip "$dictionaryFRSSourceDir/"*.zip "*.jar" -d "$dictionaryWebAppDestDir"
 unzip "$dictionaryWebAppDestDir"/*.jar "dictionary/*"  -x "dictionary/fonts/*" -d "$dictionaryWebAppDestDir"
 unzip "$dictionaryWebAppDestDir"/*.jar "Application.properties"  -d "$dictionaryWebAppDestDir"
 
+# remove any BOM characters at the beginning of directory files
+directoryFilesList=dictionary/directory*.csv
+for directoryFile in $directoryFilesList
+do
+  overwriteBOM $directoryFile BOMutf8.bin
+  overwriteBOM $directoryFile BOMutf16be.bin
+  overwriteBOM $directoryFile BOMutf16le.bin
+done
+
 # remove the jar file in the destination directory (the jar file is not needed any more)
 rm "$dictionaryWebAppDestDir"/*.jar
 
@@ -49,3 +58,33 @@ popd
 
 
 echo Done
+
+
+# function overwriteBOM: a temporary wild hack to work around problems with binary reading of UTF files
+# $1 is the file including path that is checked
+# $2 is the filename in the directory $scriptFiles that contains the BOM bytes (only file name, no directory)
+
+function overwriteBOM {
+  projWebAppDictionaries=/home/project-web/dictionarymid/htdocs/WebApp/dictionaries  ### todo
+  scriptFilesDir=$projWebAppDictionaries/scriptFiles
+  tempDir=$scriptFilesDir/tmp
+  extractFile=$tempDir/extractFile.bin
+  nullFile=$tempDir/nullFile.bin
+  withoutFirstByteFile=$tempDir/withoutFirstByteFile.bin
+
+  BOMfile=$scriptFilesDir/$2
+
+  fileSize=`stat -c %s $BOMfile`
+
+  dd ibs=1 obs=1 count=$fileSize if=$1 of=$extractFile 2> /dev/null
+  cmp $extractFile $BOMfile > /dev/null
+  if [ $? -eq 0 ]
+    then
+    # replace first bytes with a 0 (this 'destroys' the BOM)
+    dd ibs=1 obs=1 count=$fileSize if=/dev/zero of=$nullFile 2> /dev/null
+    dd ibs=1 obs=1 skip=$fileSize if=$1 of=$withoutFirstByteFile 2> /dev/null
+    cat $nullFile $withoutFirstByteFile > $1
+    echo "Replaced BOM in" $1 "("$fileSize" bytes)"
+  fi
+}
+
