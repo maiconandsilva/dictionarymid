74b48adaeb16365a472b1f4ab3f062cf2fc75c4c
starssoft <starssoft@f4d0a3ea-2ba0-4f8e-b77a-cc5a5f000db1>
2014-04-26 16:02:45 +0000

    webApp Upgraded with additional features and appCacheStatus bug cleared.

--- WebApp/Apps/mini_hmi/mini_hmi.js
+++ WebApp/Apps/mini_hmi/mini_hmi.js
@@ -1,10 +1,100 @@
 /*
 DictionaryForMIDs - a free multi-language dictionary for mobile devices.
-Copyright (C) 2011 Gert Nuber (dict@kugihan.de))
+Copyright (C) 2011 Gert Nuber (dict@kugihan.de)
 
 GPL applies - see file COPYING for copyright statement.
 */
 "use strict";
+	
+<!-- This is the Script for the Visual effects and jQuery functions: -->
+$(window).load(function() {
+	$("#headerFrame").hide().delay(700).fadeIn('slow');
+	$("#searchFrame").hide().delay(700).fadeIn('slow');
+	$("#footerFrame").hide().delay(700).fadeIn('slow');
+	$("#optionFrame").hide();
+	$("#footerText2").hide();
+	$("#recentActivity1").hide();
+	$("#recentActivity2").hide();
+	$("#recentActivity3").hide();
+	$("#recentActivity4").hide();
+	$("#recentActivity5").hide();
+	$("#footerText1").hide().delay(1000).slideToggle('slow');
+	$("#footerText1").delay(2000).slideToggle('slow');
+	$("#footerText2").delay(3000).slideToggle('slow');
+	   if ($("#recentActivity1").length) {
+		$("#footerText2").delay(7000).slideToggle('fast');
+		$("#recentActivity1").delay(8000).slideToggle('fast');
+		$("#recentActivity1").delay(10500).slideToggle('fast');
+		$("#recentActivity2").delay(11500).slideToggle('fast');
+		$("#recentActivity2").delay(13000).slideToggle('fast');
+		$("#recentActivity3").delay(14000).slideToggle('fast');
+		$("#recentActivity3").delay(16500).slideToggle('fast');
+		$("#recentActivity4").delay(17500).slideToggle('fast');
+		$("#recentActivity4").delay(20000).slideToggle('fast');
+		$("#recentActivity5").delay(21000).slideToggle('fast');
+		$("#recentActivity5").delay(23500).slideToggle('fast');
+		$("#footerText2").delay(34000).slideToggle('slow');
+	   }
+});
+
+<!-- This is the Script for feeding recent activities to Information Bar: -->
+$(document).ready(function () {
+   if (navigator.onLine){
+	$.getScript( '../../Apps/mini_hmi/zrssfeed.js', function() {
+	   $('#recentFeeds').rssfeed('http://sourceforge.net/p/dictionarymid/activity/feed', {
+		limit: 5,
+		dateformat: 'dd MMMM yyyy'
+	   });
+	});
+   }
+});
+
+$(function(){
+	$("td.optionbuttonStyle").click(function(){
+		$("#optionFrame").slideToggle();
+	});
+});
+
+<!-- This is the Script for focusing on input box and set action for enter key: -->
+
+for (var start = 1; start < 10; start++)
+	setTimeout(function Focus(){
+		document.getElementById("userInput").focus();  //Focus Input box
+	}, 350 * start);  //Timer to handle smooth focusing
+			
+function visibilityOnload(){
+	document.getElementById("headerFrame").style.visibility = "visible";  //Change Frame visibility status
+	document.getElementById("optionFrame").style.visibility = "visible";  //Change Frame visibility status
+	document.getElementById("searchFrame").style.visibility = "visible";  //Change Frame visibility status
+	document.getElementById("footerFrame").style.visibility = "visible";  //Change Frame visibility status
+}
+			
+function enterKeyCommand(){
+	if (event.keyCode == 13){
+		startTranslation(); //Enter key press action	
+	}
+}
+
+<!-- This is the Script for toggle between up/down option symbols: -->
+var open = '&#9660; Options'  //The String 'options' with down arrow	
+var close = '&#9650; Options'  //The String 'options' with up arrow
+
+var currentState = 0;
+
+function toggleoptionString() {
+	if (currentState == 0) {
+		document.getElementById("optionString").innerHTML = close;
+		currentState = 1;
+	} else {
+		document.getElementById("optionString").innerHTML = open;
+		currentState = 0;
+	}
+}
+
+
+<!-- Initialization -->
+window.addEventListener("load", initializeApplication);
+
 
 function newTranslationResult(resultOfTranslation) {
 	for (var currentTranslation = 0; 
@@ -67,6 +157,25 @@ function buildBooleanArray(numberOfElements, indexTrueElement) {
 	return booleanArray;
 }
 
+function searchHits() {
+	var optionFrame = window.frames["optionStyle"];
+	var radioButtonCount = optionFrame.document.getElementById('searchHitsRadioButton').value;
+	var comboBoxCount = optionFrame.document.getElementById('searchHitsComboBox').value;
+	var resultsCount = "50";
+		if(numberOfSearchableInputLanguages < 2){
+			resultsCount = "50";
+			return resultsCount
+		}
+	else if ((numberOfSearchableInputLanguages == 2) && (numberOfAvailableLanguages == 2)) {
+			resultsCount = radioButtonCount;
+			return resultsCount
+		}
+	else {
+			resultsCount = comboBoxCount;
+			return resultsCount
+		}
+}
+
 function startTranslation() {
 	var textInput = document.getElementById('userInput').value;
 	var textInputWithAstrik = textInput.replace(' ','* ');
@@ -76,7 +185,7 @@ function startTranslation() {
 	var outputLanguages = buildBooleanArray(dictionary.numberOfAvailableLanguages,
 											userSettingsObj.getOutputLanguage());
 	var executeInBackground = false;
-	var maxHits = 100;
+	var maxHits = searchHits();
 	var durationForCancelSearch = 5000;  // 5 seconds
 	
 	var translationParameters = 
@@ -91,6 +200,7 @@ function startTranslation() {
 		TranslationExecution.executeTranslation(translationParameters,
 		                                        deletePreviousTranslationResult,
 												newTranslationResult);
+alert(maxHits);
 }
 
 function outputMessageInAlert(message) {
@@ -98,40 +208,25 @@ function outputMessageInAlert(message) {
 }
 
 function applicationCacheEventHandler(event) {
-	if (optionWindow != null) {
-		if (! optionWindow.closed) {
-			if (optionWindowInitializationComplete) {
-				if (optionWindow.displayApplicationEvent != undefined) {
-					optionWindow.displayApplicationEvent(event.type,
-														 applicationCache.status);
-				}
-			}
-		}
-	}
+		optionWindowObj.displayApplicationEvent(event, parent.applicationCache.status);
 }
 
-function openOptionWindow() { 
-	optionWindowInitializationComplete = false;
-	optionWindow = open(pathToHTML + "OptionWindow.html", 
-	"Options", 
-	"location=no,height=300,width=200,left=100,top=100,toolbar=no,scrollbars=yes,resizable=yes");
-	optionWindow.focus();
-}
 
 function initializeApplication() {
+	optionWindowObj = window.frames["optionStyle"];
 	UtilJs.outputMessage = outputMessageInAlert;
 	applicationCache.onchecking    	= applicationCacheEventHandler;
 	applicationCache.onerror       	= applicationCacheEventHandler;
 	applicationCache.onnoupdate    	= applicationCacheEventHandler;
 	applicationCache.ondownloading 	= applicationCacheEventHandler;
-	applicationCache.onprogress 	= applicationCacheEventHandler;
+	applicationCache.onprogress 		= applicationCacheEventHandler;
 	applicationCache.oncached 		= applicationCacheEventHandler;
-	applicationCache.onobsolete 	= applicationCacheEventHandler;
-	this.translationResultsTable 	= document.getElementById("translationResultsTable");
+	applicationCache.onobsolete 		= applicationCacheEventHandler;
+	this.translationResultsTable 		= document.getElementById("translationResultsTable");
 	this.userSettingsObj 			= new UserSettings(location.href);
 	this.optionWindowInitializationComplete = false;
-	var optionWindowObj = window.frames["optionStyle"];
 	optionWindowObj.initializeOptionWindow();
+	optionWindowObj.focus();
 }
 
-var optionWindow = null;
+var optionWindowObj = null;
